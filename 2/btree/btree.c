eaed85e7dd0e106a488a102afeaf25076a34f6a1
Onegen <84882649+Onegenimasu@users.noreply.github.com>
2022-11-04 22:02:57 +0100

    make(btree): upload utils and add them to Makefile

--- 2/btree/btree.c
+++
@@ -1,9 +0,0 @@
-#include "btree.h"
-#include <stdio.h>
-
-/*
- * Pomocná funkcia ktorá vypíše uzol stromu.
- */
-void bst_print_node(bst_node_t *node) {
-  printf("[%c,%d]", node->key, node->value);
-}
